9033f1715ea6a28903839a2d8510c8742b79b164
Matheus Camargo <127213790+mathabes@users.noreply.github.com>
2023-05-08 11:34:14 -0300

    Updating ex_05.py

--- ex_05.py
+++ ex_05.py
@@ -1,12 +1,13 @@
-#ERRADO
-def primo(x):
+def primo(x: int) -> None:
     div = 0
     for cont in range(1, x):
         if x % cont == 0:
             div += 1
-        if div == 2:
-            return print("true")
-        else:
-            return print("false")
+    if x != 1 and div < 2:
+        print("true")
+    else:
+        print("false")
+        
+        
 n = int(input("Digite um número: "))
 primo(n)
